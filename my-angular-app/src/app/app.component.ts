336602a398bf15163f7f1aa2d21aea3ee454ffc6
sharonNissanov <58398243+sharonNissanov@users.noreply.github.com>
2024-05-09 18:09:51 +0300

    remove test app

--- my-angular-app/src/app/app.component.ts
+++ my-angular-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,25 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'my-angular-app';
+
+export class AppComponent implements OnInit {
+  title = 'weather app';
+  weatherData: any;
+
+  constructor(private weatherService: WeatherService){
+    //console.log(this.weatherService.getData)
+  }
+
+  ngOnInit(): void {
+    //this.getWeatherData('New York');
+  }
+
+  // getWeatherData(city: string): void {
+  //   this.weatherService.getWeather(city)
+  //     .subscribe(data => {
+  //       this.weatherData = data;
+  //       console.log(this.weatherData); 
+  //     });
+  // }
+ 
 }
