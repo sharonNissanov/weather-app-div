ac2dee4d2df03be60c68b58c37244db5a417f51b
sharonNissanov <58398243+sharonNissanov@users.noreply.github.com>
2024-05-09 21:35:57 +0300

    add isValidId

--- weather.js
+++ weather.js
@@ -1,21 +1,28 @@
-
-(function readData(id = -1){
+readData('sharon');
+function readData(id = -1){
     
+    let valid = isValidId(id);
     const API_KEY = '2cc48dd34be6452386a130925240905';
     let inputData;
     let test = 'http://api.weatherapi.com/v1/current.json?key=2cc48dd34be6452386a130925240905&q=London&aqi=no'
     
 
-    let baseUrl = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
+    //let baseUrl = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
+    let baseUrl = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}`;
     //let params = `&q=London&aqi=no`;
-    let params = `&q=48.8567,2.3508&aqi=no`;
-    console.log(baseUrl+params,   id )
+    let params = `&q=48.8567,2.3508&aqi=no&days=14`;
+    console.log(baseUrl+params,   id , valid )
     getWeatherData(baseUrl+params)
     
 
-
-    function isValidId(){
-
+    //gets an id and return true if there is an ele with this id
+    function isValidId(id){
+        if(id == null  || id == undefined)
+            return false;
+        if(document.getElementById(id) || null ){
+            return true
+        }
+        return false;
     }
 
     function addInput(){
@@ -57,4 +64,4 @@
 
     }
 
-})();
+};
