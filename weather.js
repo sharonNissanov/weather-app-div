d7f9b963f66a6bb2272a50b67d9ae3f972010bf7
sharonNissanov <58398243+sharonNissanov@users.noreply.github.com>
2024-05-09 20:48:10 +0300

    add functions

--- weather.js
+++ weather.js
@@ -4,25 +4,15 @@
     const API_KEY = '2cc48dd34be6452386a130925240905';
     let inputData;
     let test = 'http://api.weatherapi.com/v1/current.json?key=2cc48dd34be6452386a130925240905&q=London&aqi=no'
-    console.log(111111, test,   id )
+    
 
-    let baseUrl = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=London&aqi=no`;
+    let baseUrl = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
+    //let params = `&q=London&aqi=no`;
+    let params = `&q=48.8567,2.3508&aqi=no`;
+    console.log(baseUrl+params,   id )
+    getWeatherData(baseUrl+params)
+    
 
-   
-    fetch(baseUrl)
-    .then(response => {
-      
-        if (!response.ok) {
-        throw new Error('Network response was not ok');
-        }
-        return response.json(); 
-    })
-    .then(data => {
-        console.log(data); 
-    })
-    .catch(error => {
-        console.error('ERROR:', error);
-    });
 
     function isValidId(){
 
@@ -36,6 +26,22 @@
 
     }
 
+    function getWeatherData(url){
+        fetch(url)
+        .then(response => {
+          
+            if (!response.ok) {
+            throw new Error('Network response was not ok');
+            }
+            return response.json(); 
+        })
+        .then(data => {
+            console.log(data); 
+        })
+        .catch(error => {
+            console.error('ERROR:', error);
+        });
+    }
     // Get (don’t show) the weather for the user’s entered location in the next 2 weeks
     function getUserLocation(){
 
@@ -50,4 +56,5 @@
     function createWeatherCard(){
 
     }
+
 })();
