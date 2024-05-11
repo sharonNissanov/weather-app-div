fc56a5795fa85a41d6ba8244dd71668e5ba1e681
sharonNissanov <58398243+sharonNissanov@users.noreply.github.com>
2024-05-11 21:08:28 +0300

    add docu

--- script.ts
+++ script.ts
@@ -6,6 +6,13 @@ function injectScriptIntoDiv(targetID = ""){
 
 var targetElement = getTargetElement(targetID);
 
+/**
+ * Retrieves the target element based on the provided ID.
+ * If the provided ID is valid and corresponds to an existing element, returns that element;
+ * otherwise, returns the body element.
+ * @param {string} targetID - The ID of the target element to retrieve.
+ * @returns {HTMLElement} - The target element if found, otherwise the body element.
+ */
 	function getTargetElement(targetID) {
 		let valid = isValidId(targetID);
 		if(typeof targetID === 'string' && valid){ 
@@ -13,6 +20,11 @@ var targetElement = getTargetElement(targetID);
 		}
 		return document.getElementsByTagName('body')[0];
 	
+	/**
+     * Checks if the provided ID is a valid non-empty string.
+     * @param {string} targetID - The ID to validate.
+     * @returns {boolean} - True if the ID is valid and corresponds to an existing element, otherwise false.
+     */
 		function isValidId(targetID) {
 			// Check if the id is a non-empty string
 			if (typeof targetID !== 'string' || targetID.trim() === '' || targetID == null ) {
@@ -22,7 +34,12 @@ var targetElement = getTargetElement(targetID);
 		}
 	}
 
-	(function getData(){
+	/**
+ * Injects an iframe containing the content of a specified URL into the target element.
+ * The injected iframe is styled to match the dimensions of the target element.
+ * @returns {void}
+ */
+	(function injectWithIframe(){
 	console.log(targetElement)
 	const style = `
 		#iframeContainer {
