a8362dd3ac6657d92476e3afbbef99df8365def7
sharonNissanov <58398243+sharonNissanov@users.noreply.github.com>
2024-05-13 08:22:07 +0300

    appendDynamicWeather.ts

--- solutions/appendDynamicWeather.js
+++ solutions/appendDynamicWeather.js
@@ -1,48 +1,46 @@
-
 // Invokes (append) the function to inject a script into a specified div element.
 injectScriptIntoDiv();
-
 /**
  * Injects a script into a specified div element identified by its ID.
  * If no ID is provided from url or the ID is invalid, the script is injected into the body.
  * @returns {void}
  */
-function injectScriptIntoDiv(){
-	const scriptSrc = document.currentScript.src;
-	const url = new URL(scriptSrc);
-    const targetID = url.searchParams.get('targetID');
-	var targetElement = getTargetElement(targetID);
-	injectWithIframe(targetElement);
-};
-
+function injectScriptIntoDiv() {
+    var scriptElement = document.currentScript;
+    var scriptSrc = scriptElement === null || scriptElement === void 0 ? void 0 : scriptElement.getAttribute('src');
+    // Check if scriptSrc is not null or undefined
+    if (scriptSrc) {
+        var url = new URL(scriptSrc);
+        var targetID = url.searchParams.get('targetID');
+        var targetElement = getTargetElement(targetID);
+        console.log("url: ", url);
+        console.log("targetID: ", targetID, "targetElement: ", targetElement);
+        injectWithIframe(targetElement);
+    }
+}
+;
 /**
  * Injects an iframe containing the content of a specified URL into the target element.
  * The injected iframe is styled to match the dimensions of the target element.
  * The source of the iframe is the weather app deployed on Netlify.
- * @param {HTMLElement} targetElement - the target element.
+ * @param {HTMLElement | null} targetElement - the target element.
  * @returns {void}
  */
 function injectWithIframe(targetElement) {
-
-    let iframeContainer = document.createElement('div');
-    let iframe = document.createElement('iframe');
-
+    var iframeContainer = document.createElement('div');
+    var iframe = document.createElement('iframe');
     // Set styles for the iframe container and iframe
     setIframeContainerStyle();
     setIframeStyle();
-
     // Append the iframe to the container
     iframe.src = "https://dynamicweatherapp14.netlify.app/";
-
     iframeContainer.appendChild(iframe);
-    
     // Append the new div to the document body
-    if(targetElement !== null){
+    if (targetElement !== null) {
         targetElement.append(iframeContainer);
     }
-    
     // Function to set styles for the iframe container
-    function setIframeContainerStyle(){
+    function setIframeContainerStyle() {
         iframeContainer.id = "iframeContainer";
         iframeContainer.style.paddingBottom = '16%';
         iframeContainer.style.position = 'relative';
@@ -51,9 +49,8 @@ function injectWithIframe(targetElement) {
         iframeContainer.style.overflow = 'hidden';
         iframeContainer.style.zIndex = '10000';
     }
-
-   // Function to set styles for the iframe
-    function setIframeStyle(){
+    // Function to set styles for the iframe
+    function setIframeStyle() {
         iframe.setAttribute("frameborder", "0");
         iframe.setAttribute("allowfullscreen", "");
         iframe.style.position = 'absolute';
@@ -64,29 +61,27 @@ function injectWithIframe(targetElement) {
         iframe.style.border = 'none';
     }
 }
-
 /**
  * Returns the target element based on the provided ID. If the provided ID is valid and
  * corresponds to an existing element, returns that element; otherwise, returns the body element.
  * @param {string} targetID - The ID of the target element.
- * @returns {HTMLElement} - The target element if found, otherwise the body element.
+ * @returns {HTMLElement | null} - The target element if found, otherwise the body element.
  */
 function getTargetElement(targetID) {
-    let valid = isValidId(targetID);
-    if(typeof targetID === 'string' && valid){ 
+    var valid = isValidId(targetID);
+    if (typeof targetID === 'string' && valid) {
         return document.getElementById(targetID);
     }
     return document.getElementsByTagName('body')[0];
 }
-
 /**
  * Checks if the provided ID is a valid non-empty string.
  * @param {string} targetID - The ID to validate.
  * @returns {boolean} - True if the ID is valid and corresponds to an existing element, otherwise false.
  */
-function isValidId(targetID) {    
-    if (typeof targetID !== 'string' || targetID.trim() === '' || targetID == null ) {
+function isValidId(targetID) {
+    if (typeof targetID !== 'string' || targetID.trim() === '' || targetID == null) {
         return false;
-    } 
+    }
     return document.getElementById(targetID) !== null;
 }
